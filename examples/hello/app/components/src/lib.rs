5d1cdced8ad3610ae20b59ebe0eb2b261ef342a2
norman784 <normanpaniagua@gmail.com>
2021-11-10 14:21:19 +0100

    updated example (still broken)

--- examples/hello/app/components/src/lib.rs
+++ examples/hello/app/components/src/lib.rs
@@ -1,35 +1,22 @@
-use std::borrow::Cow;
+use etagere::prelude::*;
 
-use etagere::*;
-use models::*;
-
-#[derive(Default)]
-pub struct PostComponent<'a> {
-  post: Post<'a>,
-}
-
-impl<'a> view::CustomElement<'a> for PostComponent<'a> {
-  type Attributes = Post<'a>;
-
-  fn create(&mut self, attributes: Self::Attributes, _: view::Html<'a>) {
-    self.post = attributes;
-  }
-
-  fn attributes(&self) -> Self::Attributes {
-    self.post.clone()
-  }
-
-  fn render(&self) -> view::Html<'a> {
-    html! {
-      <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">{ self.post.title }</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="lead mb-4">{ self.post.body }</p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <img src={ self.post.image } />
-          </div>
-        </div>
+#[component]
+pub fn PostComponent<'a>(title: &'a str, body: &'a str, image: Option<&'a str>) {
+  html! {
+    <div class="px-4 py-5 my-5 text-center">
+      <h1 class="display-5 fw-bold">{ title }</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">{ body }</p>
+        { if let Some(image) = image {
+          html! {
+            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+              <img src={ image } />
+            </div>
+          }
+        } else {
+          None
+        }}
       </div>
-    }
+    </div>
   }
 }
--- examples/hello/app/models/src/lib.rs
+++ examples/hello/app/models/src/lib.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Clone, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct Post<'a> {
   pub id: usize,
   pub title: &'a str,
