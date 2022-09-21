c60ac87796b707ed19eb7092570a6c9e986da653
norman784 <normanpaniagua@gmail.com>
2022-09-21 21:39:14 +0200

    simplified api example

--- examples/api/src/main.rs
+++ examples/api/src/main.rs
@@ -6,25 +6,10 @@ mod api;
 
 #[cfg(not(target_arch = "wasm32"))]
 async fn server(router: axum::Router) -> Result<(), String> {
-  async fn handle_error(_err: std::io::Error) -> impl axum::response::IntoResponse {
-    (
-      axum::http::StatusCode::INTERNAL_SERVER_ERROR,
-      "Something went wrong...",
-    )
-  }
-
   axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-    .serve(
-      router
-        .fallback(
-          axum::routing::get_service(tower_http::services::ServeDir::new("./public"))
-            .handle_error(handle_error),
-        )
-        .into_make_service(),
-    )
+    .serve(router.into_make_service())
     .await
     .unwrap();
-
   Ok(())
 }
 
